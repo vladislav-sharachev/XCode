3f91be1d44183939db08592c3453cb2389b7f6bf
vladislav-sharachev <52072721+vladislav-sharachev@users.noreply.github.com>
2020-10-06 22:46:42 +0400

    images controller

---
+++ ImagesController.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class ImagesController: UITableViewController {
+
+    var list: [String] = [String]()
+    let newUrl = URL(string: "https://placehold.it/375x150?text=")!
+    
+    func myURL(imageIndex index: Int) -> String {
+        let url = "https://placehold.it/375x150?text=\(String(describing: index))"
+        return url
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        downloadImage(withURL: newUrl, forCell: ImagesCell())
+    }
+    
+    func downloadImage(withURL url: URL, forCell cell: UITableViewCell) {
+        let url = url.absoluteString
+        let index = 1
+        for _ in 1...100 {
+            list.append(url + "\(index)")
+        }
+        self.tableView.reloadData()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ImagesCell
+        cell.configure(url: list[indexPath.row])
+        return cell
+    }
+}
